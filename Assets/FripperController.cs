79fc63e142d201f2e156af00db9a041d1507c295
Velka-code <nadaya081@gmail.com>
2021-05-18 09:18:10 +0900

    Initial commit

---
+++ Assets/FripperController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour
+{
+    //HingeJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20;
+    //弾いた時の傾き
+    private float flickAngle = -20;
+
+    // Use this for initialization
+    void Start()
+    {
+        //HingeJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //左矢印キーまたAキーを押した時左フリッパーを動かす
+        if ((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+        //右矢印キーまたはDキーを押した時右フリッパーを動かす
+        if ((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        //下矢印キーまたはSキーを押した時両フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            SetAngle(this.flickAngle);
+        }
+
+
+        //矢印キー離された時フリッパーを元に戻す
+        if ((Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A)) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        if ((Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D)) && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
+        {
+            SetAngle(this.defaultAngle);
+        }
+
+        //キー操作とタッチ操作を別々に記載（タッチ用）
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began://タッチしたときフリッパーを動かす
+                    float getPos = touch.position.x;
+
+                    if ((getPos < Screen.width / 2) && tag == "LeftFripperTag")
+                    {
+                        SetAngle(this.flickAngle); 
+                    }
+                    if ((getPos >= Screen.width / 2) && tag == "RightFripperTag")
+                    {
+                        SetAngle(this.flickAngle); 
+                    }
+                    break;
+
+                case TouchPhase.Ended: //タッチを離したときフリッパーを元に戻す
+                    float getPosa = touch.position.x;
+
+                    if ((getPosa < Screen.width / 2) && tag == "LeftFripperTag")
+                    {
+                        SetAngle(this.defaultAngle);
+                    }
+                    if ((getPosa >= Screen.width / 2) && tag == "RightFripperTag")
+                    {
+                        SetAngle(this.defaultAngle);
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+
+    //フリッパーの傾きを設定
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
---
+++ Assets/Score.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    //
+    private int score = 0;
+    private GameObject scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.scoreText = GameObject.Find("ScoreText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log(collision.collider.tag);
+        switch (collision.collider.tag)
+        {
+            case "SmallStarTag":
+                this.score += 10;
+
+                break;
+
+            case "LargeStarTag":
+                this.score += 20;
+               
+                break;
+
+            case "SmallCloudTag":
+                this.score += 10;
+                
+                break;
+
+            case "LargeCloudTag":
+                this.score += 20;
+               
+                break;
+
+            default:
+                break;
+        }
+
+        this.scoreText.GetComponent<Text>().text = "Score " + this.score + "pt";
+
+    }
+}
